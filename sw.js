478fb306a744a091e48ca3b26184a7345f85e962
naimulhudanimon-eng <naimulhudanimon@gmail.com>
2026-03-13 15:22:49 +0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,70 @@
+const CACHE_NAME = "selftracker-v1";
 
+const urlsToCache = [
+"./",
+"./index.html"
+];
+
+self.addEventListener("install", event => {
+
+self.skipWaiting();
+
+event.waitUntil(
+
+caches.open(CACHE_NAME)
+
+.then(cache => cache.addAll(urlsToCache))
+
+);
+
+});
+
+self.addEventListener("activate", event => {
+
+event.waitUntil(
+
+caches.keys().then(keys => {
+
+return Promise.all(
+
+keys.map(key => {
+
+if (key !== CACHE_NAME) {
+
+return caches.delete(key);
+
+}
+
+})
+
+);
+
+})
+
+);
+
+self.clients.claim();
+
+});
+
+self.addEventListener("fetch", event => {
+
+event.respondWith(
+
+fetch(event.request)
+
+.then(response => {
+
+const clone = response.clone();
+
+caches.open(CACHE_NAME).then(cache => cache.put(event.request, clone));
+
+return response;
+
+})
+
+.catch(() => caches.match(event.request))
+
+);
+
+});
